daa3a2bf5a3bdbd32bd1436f4ca2946ef925a158
Nick Hurtado and Zach Hedgepeth <nick@>
2016-08-11 16:27:02 -0400

    complete the celeb match web page

--- js/scripts.js
+++ js/scripts.js
@@ -14,16 +14,23 @@ $(document).ready(function() {
 
     alert("You are too young for this site");
 
-  } else if (gender === 'male') {
+  }
+
+  else {
+
+   if (gender === 'male') {
     // at this point, we know we have a male, over 18
     if (color === 'light') {
       // any code in here knows that we have a male, over 18 whose favorite color is a light color
       if (cuisine === 'american') {
         // go eat american food!
+        $("#jennifer").show();
       } else if (cuisine === 'chinese') {
         // go eat chinese food, only if american was not chosen.
+        ("#angelina").show();
       } else if (cuisine === 'mediterranean') {
         // go eat mediterranean, ONLY if american AND chinese were not chosen.
+      $("#angelina").show();
       } else {
         // go eat latin, only if none of the other options were chosen.
         // here, we know over 18, male, light, latin food
@@ -33,10 +40,13 @@ $(document).ready(function() {
       // male, over 18, likes dark colors.
       if (cuisine === 'american' || cuisine === 'chinese') {
         //male, over 18, likes dark colors, eats american or chinese
+        $("#lopez").show();
       } else if (cuisine === 'mediterranean') {
         //male male, over 18, likes dark colors, eats mediterranean ONLY if American and chinese are not chosen
+        $("#lopez").show();
       } else {
         // over 18, male, dark, latin food.
+        ("#angelina").show();
       }
     }
 
@@ -44,30 +54,32 @@ $(document).ready(function() {
     // female, over 18.
     if (color === 'light'){
       //female, over 18, light colors
-      {
-        if (cuisine === 'mediterranean' || cuisine === 'latin')
+        if (cuisine === 'mediterranean' || cuisine === 'latin') {
         //female, over 18, light, likes mediterranean OR latin NOT the others
+        $("#brad").show();
       } else if (cuisine === 'american') {
         //female, over 18, light, likes american
+        $("#will").show();
       } else {
         //female, over 18, light, likes chinese
+        $("#chow").show();
       }
-    } else (color === 'dark') {
+    }
+
+  else {
       //female, over 18, dark colors
         if (cuisine === 'american' || cuisine === 'chinese') {
         //female, over 18, dark, likes american and chinese
+        $("#chow").show();
       } else if (cuisine === 'latin') {
         //female, over 18, dark, likes latin
+        $("#will").show();
       } else {
         //femal, over 18, dark, likes mediterranean
+        $("#brad").show();
       }
-
-
     }
-
-
   }
-
+}
 });
-
 });
